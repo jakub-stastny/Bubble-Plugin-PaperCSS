536b73029bc15e564a6f428cbaf5f17146897f2d
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2022-10-04 16:20:43 +0200

    Automatic commit from Bubble

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -3,4 +3,11 @@ function(instance, context) {
     
     instance.canvas.append(button)
     instance.data.button = button
+    
+    button.style.width = "100%"
+    button.style.height = "100%"
+
+    button.addEventListener("click", function () {
+        instance.triggerEvent("clicked")
+    })
 }
--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -2,7 +2,4 @@ function(instance, properties, context) {
     const { button } = instance.data
 
     button.innerText = properties.label
-    
-    button.style.width = "100%"
-    button.style.height = "100%"
 }
