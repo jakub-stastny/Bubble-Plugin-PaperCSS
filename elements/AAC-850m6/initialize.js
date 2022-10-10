7ba91f8475853c1567044223a1f5c00d47f096d6
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2022-10-10 18:36:15 -0500

    Automatic commit from Bubble

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -5,7 +5,17 @@ function(instance, context) {
     instance.data.button = button
     
     button.style.width = "100%"
-    button.style.height = "80%" //"100%"
+    //button.style.height = "100%" // "80%" //"100%"
+    button.style.marginBottom = "29px"
+    button.style.marginRight = "29px"
+
+    //console.log(instance.canvas.style.height)
+    //delete instance.canvas.style.height
+    instance.canvas.style.height = "fit-content" //`${button.offsetHeight}px`
+    //console.log([button.offsetHeight, instance.canvas.style.height])
+    
+    //instance.canvas.style.display = "flex"
+    //instance.canvas.style.flexDirection = "row"
 
     button.addEventListener("click", function () {
         instance.triggerEvent("clicked")
