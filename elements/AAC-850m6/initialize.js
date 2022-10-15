db7d3f776a0dc8b5e48b93f7907f9443e7f63235
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2022-10-15 16:49:25 -0500

    Automatic commit from Bubble

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -1,15 +1,11 @@
 function(instance, context) {
-    const button = document.createElement("button")
+    const button = element("button", {}, {width: "100%", height: "100%"})
     
     instance.canvas.append(button)
     instance.data.button = button
-    
-    button.style.width = "100%"
-    button.style.height = "100%" // "80%"
 
     // Allow space for the shadow.
-    button.style.marginBottom = "29px"
-    button.style.marginRight = "29px"
+    button.style.marginRight = button.style.marginBottom = "29px"
 
     // Without this the button overflows the parent div, so it looks chopped off.
     instance.canvas.style.height = "fit-content" //`${button.offsetHeight}px`
--- elements/AAC-850m6/preview.js
+++ elements/AAC-850m6/preview.js
@@ -2,9 +2,8 @@ function(instance, properties) {
     const button = document.createElement("button")
 
     button.innerText = properties.label
-    button.style.width = properties.bubble.width() + "px"
-    button.style.height = properties.bubble.height() + "px"
-    button.classList.add("btn-" + properties.type)
+    button.style.width = `${properties.bubble.width()}px`
+    button.style.height = `${properties.bubble.height()}px`
 
     instance.canvas.append(button)
 }
