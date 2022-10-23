1a7525a3e22396caeeb81f144c58c38972b339bf
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2022-10-23 14:37:37 -0500

    Fix

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -12,19 +12,16 @@
  */
 function(instance, context) {
   //const button = element("button", {}, {width: "100%", height: "100%"})
-  const button = element("button", {}, {width: "fit-content", height: "fit-content", whiteSpace: "nowrap"})
+  //const button = element("button", {}, {width: "fit-content", height: "fit-content", whiteSpace: "nowrap"})
+  const button = element("button", {}, {whiteSpace: "nowrap", marginRight: "29px", marginBottom: "29px"})
  
   instance.canvas.append(button)
   instance.data.button = button
 
-  // Allow space for the shadow.
-  button.style.marginRight = button.style.marginBottom = "29px"
-
   // Without this the button overflows the parent div, so it looks chopped off.
   // With it though, content is cut. For instance if I name the button "Updated button", it'll only show "Updated".
-  //instance.canvas.style.width = "fit-content" //`${button.offsetHeight}px`
-  //delete instance.canvas.style.minWidth
-  //delete instance.canvas.style.minHeight
+  instance.canvas.style.width = instance.canvas.style.height = "fit-content" //`${button.offsetHeight}px`
+
   if (instance.canvas.style.minWidth === instance.canvas.style.maxWidth) {
     instance.canvas.style.minWidth = instance.canvas.style.maxWidth = null
   }
