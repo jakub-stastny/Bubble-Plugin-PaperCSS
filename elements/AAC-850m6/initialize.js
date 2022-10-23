cf6a76936a0f0de093850b24e24c6981afa39a48
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2022-10-23 15:45:06 -0500

    Refactoring

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -1,19 +1,5 @@
-/*
- * How should the sizing work:
- * 1/ Responsive mode (default)
- *    Fit to the size of content.
- *    Fill the parent container (iPhone screen for instance).
- *    Should it be resizable at all?
- *    Probably a select box with sizing strategy is in order? Or is there a way to detect this, like if the user switched to the responsive designer or how does it work?
- *
- * 2/ Specified dimensions
- *    What happens on various screen sizes?
- *.    In the editor one is also able to set min/max width/height and margin.
- */
 function(instance, context) {
-  //const button = element("button", {}, {width: "100%", height: "100%"})
-  //const button = element("button", {}, {width: "fit-content", height: "fit-content", whiteSpace: "nowrap"})
-  const button = element("button", {}, {whiteSpace: "nowrap", /*marginRight: "29px", marginBottom: "29px",*/ overflow: "visible"})
+  const button = element("button", {}, {whiteSpace: "nowrap", overflow: "visible"})
  
   instance.canvas.append(button)
   instance.data.button = button
@@ -21,18 +7,15 @@ function(instance, context) {
   instance.canvas.style.overflow = "visible"
 
   // Fix bottom shadow.
+  // By doing so, we make the height always automatic and unchangeable.
   instance.canvas.style.height = "fit-content"
   instance.canvas.style.minHeight = instance.canvas.style.maxHeight = null
 
   if (instance.canvas.style.width === "calc(100% + 0px)") {
-    // Stretch
+    // Stretch on say smartphone screens.
     button.style.width = "100%"
   } else {
     instance.canvas.style.width = "fit-content"
-  }
-
-  if (instance.canvas.style.minWidth === instance.canvas.style.maxWidth) {
-    //instance.canvas.style.minWidth = instance.canvas.style.maxWidth = null
   }
 
   button.addEventListener("click", () => instance.triggerEvent("clicked"))
