96b9eb119fdc31dc58f4b8112ea5eff77bc51c3a
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2022-10-24 09:05:34 -0500

    Log errors

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -1,23 +1,25 @@
 function(instance, context) {
-  const button = element("button", {}, {
-    whiteSpace: "nowrap", overflow: "visible"})
+  logErrors("PaperCSS.button.initialize", () => {
+    const button = element("button", {}, {
+      whiteSpace: "nowrap", overflow: "visible"})
 
-  // Fix bottom shadow.
-  // By doing so, we make the height always automatic and unchangeable.
-  updateStyle(instance.canvas, {
-    overflow: "visible", height: "fit-content",
-    minHeight: null, maxHeight: null})
+    // Fix bottom shadow.
+    // By doing so, we make the height always automatic and unchangeable.
+    updateStyle(instance.canvas, {
+      overflow: "visible", height: "fit-content",
+      minHeight: null, maxHeight: null})
 
-  if (instance.canvas.style.width === "calc(100% + 0px)") {
-    // Stretch on say smartphone screens.
-    updateStyle(button, {width: "100%"})
-  } else {
-    updateStyle(instance.canvas, {width: "fit-content"})
-  }
+    if (instance.canvas.style.width === "calc(100% + 0px)") {
+      // Stretch on say smartphone screens.
+      updateStyle(button, {width: "100%"})
+    } else {
+      updateStyle(instance.canvas, {width: "fit-content"})
+    }
 
-  // Register.
-  register(instance, {button})
+    // Register.
+    register(instance, {button})
 
-  // Events.
-  button.addEventListener("click", () => instance.triggerEvent("clicked"))
+    // Events.
+    button.addEventListener("click", () => instance.triggerEvent("clicked"))
+  })
 }
--- elements/AAD-850m8/initialize.js
+++ elements/AAD-850m8/initialize.js
@@ -4,28 +4,30 @@
 //		 <span>This is the second check</span>
 
 function(instance, context) {
-	// Without this the button overflows the parent div, so it looks chopped off.
-  updateStyle(instance.canvas, {width: "fit-content", height: "fit-content"})
+  logErrors("PaperCSS.checkbox.initialize", () => {
+    // Without this the button overflows the parent div, so it looks chopped off.
+    updateStyle(instance.canvas, {width: "fit-content", height: "fit-content"})
 
-	// Create DOM.
-	const topNode = fieldset({class: "form-group"}, {width: "100%", height: "100%"}, () => (
-		label({class: "paper-check"}, {}, () => ([
-			input({
-				type: "checkbox"
-			}, {
-				display: "inline-block",
-				marginRight: "7px"
-		    }),
-			span()
-		]))
-	))
+  	// Create DOM.
+  	const topNode = fieldset({class: "form-group"}, {width: "100%", height: "100%"}, () => (
+  		label({class: "paper-check"}, {}, () => ([
+  			input({
+  				type: "checkbox"
+  			}, {
+  				display: "inline-block",
+  				marginRight: "7px"
+  		    }),
+  			span()
+  		]))
+  	))
 
-	// Register.
-  register(instance, {topNode})
+  	// Register.
+    register(instance, {topNode})
 
-	// Events.
-  checkbox.addEventListener("change", (event) => {
-    instance.triggerEvent("clicked")
-    instance.triggerEvent(event.target.checked ? "checked" : "unchecked")
+  	// Events.
+    checkbox.addEventListener("change", (event) => {
+      instance.triggerEvent("clicked")
+      instance.triggerEvent(event.target.checked ? "checked" : "unchecked")
+    })
   })
 }
