0f6cf3d88dd532741a85e1cc3d066f6328292518
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2022-10-24 09:31:25 -0500

    Fix

--- elements/AAC-850m6/update.js
+++ elements/AAC-850m6/update.js
@@ -1,6 +1,7 @@
 function(instance, properties, context) {
   logErrors("PaperCSS.button.update", () => {
     const { button } = instance.data
+    console.log({button, properties})
 
     button.innerText = properties.label
     button.classList.add("btn-" + properties.type)
