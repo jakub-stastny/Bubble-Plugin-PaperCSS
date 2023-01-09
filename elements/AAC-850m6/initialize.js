07f7cd7593141d3aeb8e7961a50995e0c0f85ccd
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-01-09 17:31:47 +0000

    WIP

--- elements/AAC-850m6/initialize.js
+++ elements/AAC-850m6/initialize.js
@@ -3,7 +3,8 @@ function(instance, context) {
     const button = element("button", {}, {
       whiteSpace: "nowrap", overflow: "visible"})
 
-    // Debug: show background on the canvas.
+    // Debug: show background on the canvas to make sure the element
+    // is filling the whole area of the canvas.
     instance.canvas.backgroundColor = "red"
 
     // Fix bottom shadow.
@@ -25,7 +26,8 @@ function(instance, context) {
     // Events.
     const triggerEvent = (eventName) => {
       return function () {
-        console.log(`~ Triggering ${eventName}`)
+        console.log(`~ Triggering event %c${eventName}%c.`,
+                    "color:#DC143C", "color:#000")
         instance.triggerEvent(eventName)
       }
     }
